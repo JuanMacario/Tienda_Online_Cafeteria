98e3363b07be469ca1e900e59c28afab00a624be
Juan Macario <juanmacario037@gmail.com>
2026-06-12 10:47:45 -0600

    se agrego la clase pedido en el script.js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ class Cafe {
     #precio;
     #categoria;
     #descripcion;
+    #id;
 
-    constructor(nombre, precio, categoria, descripcion) {
+    constructor(nombre, precio, categoria, descripcion, id) {
         this.#nombre = nombre
         this.#precio = precio
         this.#categoria = categoria
         this.#descripcion = descripcion
+        this.#id = id
     }
 
     get nombre() {
@@ -27,6 +29,10 @@ class Cafe {
         return this.#descripcion
     }
 
+    get id() {
+        return this.#id
+    }
+
     set nombre(value) {
         this.nombre = value
     }
@@ -41,5 +47,77 @@ class Cafe {
 
     set descripcion(value) {
         this.descripcion = value
+    }
+
+    set id(value) {
+        this.id = value
+    }
+}
+
+class Pedido {
+    #id;
+    #nombre;
+    #unitario;
+    #subTotal;
+    #cantidad;
+
+    constructor(id, nombre, unitario, subtotal, cantidad) {
+        this.#id = id
+        this.#nombre = nombre
+        this.#unitario = unitario
+        this.#subTotal = subtotal
+        this.#cantidad = cantidad
+    }
+
+    set id() {
+        return this.#id
+    }
+
+    set nombre() {
+        return this.#nombre
+    }
+
+    set unitario() {
+        return this.#unitario
+    }
+
+    set subtotal() {
+        return this.#subTotal
+    }
+
+    set cantidad() {
+        return this.#cantidad
+    }
+
+    get id(value) {
+        this.id = value
+    }
+
+    get nombre(value) {
+        this.nombre = value
+    }
+
+    get unitario(value) {
+        this.unitario = value
+    }
+
+    get subtotal(value) {
+        this.subtotal = value
+    }
+
+    get cantidad(value) {
+        this.cantidad = value
+    }
+
+    aumentaCantidad() {
+        this.cantidad += 1
+    }
+
+    disminuirProducto() {
+        this.cantidad = this.cantidad - 1
+    }
+
+    sumarSubtotal() {
+        this.subtotal = this.cantidad * this.unitario
     }
 }
